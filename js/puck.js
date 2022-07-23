1617624b940ff21cfeafb0f7ec46b4929e1c1898
Ricardo Magalhães <ricardospilotes@gmail.com>
2022-07-23 15:28:04 -0300

    Add files via upload

--- js/puck.js
+++ js/puck.js
@@ -1,7 +1,18 @@
 const item = document.querySelector(".puck");
+const item2 = document.querySelector(".puck2");
 
 item.animate(
   [{ transform: "translateX()" }, { transform: "translateY(-0.6rem)" }],
+  {
+    duration: 1700,
+    easing: "ease-in-out",
+    direction: "alternate",
+    iterations: Infinity,
+  }
+);
+
+item2.animate(
+  [{ transform: "translateX()" }, { transform: "translateY(0.6rem)" }],
   {
     duration: 1700,
     easing: "ease-in-out",
